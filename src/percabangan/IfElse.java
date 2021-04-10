a6ba83294f8a08e9a58efe1706526c84c290f72b
chandrafebri <chandra.febri14@gmail.com>
2021-04-11 01:42:07 +0800

    Percabangan dengan Statement 1 Jalan (Kondisi False)

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,12 +3,15 @@ package percabangan;
 public class IfElse {
     public static void main(String[] args) {
         double bayarSPP = 1000000;
-        double saldoTabungan = 1200000;
+        double saldoTabungan = 800000;
 
         if (saldoTabungan<bayarSPP){
             System.out.println("Uang Masih Kurang, Tunggu Uang Kaget!!");
+        }else if (saldoTabungan>bayarSPP){
+            double kembalian = saldoTabungan-bayarSPP;
+            System.out.println("Uang Cukup, Alhamdulillah, Lunas!! Ada Kembalian : "+kembalian);
         }else{
-            System.out.println("Uang Cukup, Alhamdulillah, Lunas!!");
+            System.out.println("Uang Pas..pas..an");
         }
     }
 }
