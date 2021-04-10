a6a486ee369a16330627b58badbbc83966ec0778
chandrafebri <chandra.febri14@gmail.com>
2021-04-11 01:43:19 +0800

    Percabangan dengan Statement 2 Jalan (Kondisi True)

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,7 +3,7 @@ package percabangan;
 public class IfElse {
     public static void main(String[] args) {
         double bayarSPP = 1000000;
-        double saldoTabungan = 800000;
+        double saldoTabungan = 1200000;
 
         if (saldoTabungan<bayarSPP){
             System.out.println("Uang Masih Kurang, Tunggu Uang Kaget!!");
