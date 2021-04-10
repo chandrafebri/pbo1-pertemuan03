19339f5dafa11d1b4e6ec18d0b221511b4456972
chandrafebri <chandra.febri14@gmail.com>
2021-04-11 01:02:54 +0800

    Operator Binary Aritmatika

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class OperatorBinary {
+    String nama = "Chandra Febri Ariyanto";
+    int jumlahSKS = 24;
+    double IPK = 3.99;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
